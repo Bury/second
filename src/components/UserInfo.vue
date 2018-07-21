2484ec795b0ead9f7f154d0920f579667a4cbf0d
lantian <lantian@qq.com>
2018-07-21 20:01:55 +0800

    蓝天，修改正则验证

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -63,6 +63,8 @@
 	</div>
 </template>
 <script>
+	
+	import globalRules from '../config/global_rules'
     import remindApi from '../api/remind'
     export default {
       	name:'user-info',
@@ -85,27 +87,9 @@
                 editUserInfoData:{},
                 labels:{},
                 UserInfoRules:{
-                    name: [
-                        { required: true, message: '请输入姓名', trigger: 'blur' },
-                        { min: 2, max: 4, message: '长度在 2 到 4 个字符', trigger: 'blur' }
-                    ],
-                    gender:[
-                        { required: true, message: '请选择性别', trigger: 'blur' },
-                    ],
-                    phone:[
-                        { required: true, message: '请输入手机号码', trigger: 'blur' },
-                        {
-                            validator: (rule, value, callback) => {
-                                if (value.match(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/)) {
-                                    callback();
-                                } else {
-                                    callback("请输入正确的手机号码！");
-                                }
-                            },
-                            trigger: 'blur'
-                        }
-                    ]
-
+                    name:globalRules.rules.user.truename(),
+                    gender:globalRules.rules.user.gender(),
+                    phone:globalRules.rules.user.phone(),
                 }
             };
         },
--- src/config/global_rules.js
+++ src/config/global_rules.js
@@ -4,48 +4,87 @@ const rules={
 		id(len_min,len_max,text){
 			return [
 	    		{ required: true, message: text, trigger: 'blur' },
-	        	{ min: len_min, max: len_max, message: '长度在 4 到 20 个字符', trigger: 'blur' }
+	        	{ min: len_min, max: len_max, message: `长度在 ${len_min}到${len_max}个字符`, trigger: 'blur' }
 	    	]
 		},
 	},
 
 	user:{
-
+		
+        //用户名验证
 		username(len_min,len_max,text){
 			return [
 	    		{ required: true, message: text, trigger: 'blur' },
-	        	{ min: len_min, max: len_max, message: '长度在 4 到 20 个字符', trigger: 'blur' }
+	        	{
+	        		validator:(rule,value,callback) =>{
+	        			if(value.match(/^[a-zA-Z0-9]{5,16}$/)){
+	        				callback();
+	        			}else{
+	        				callback("只能是数字和字母5-16位")
+	        			}
+	        		},
+	        		trigger:'blur'
+	        	}
 	    	]
 		},
-
+        
+        //密码验证
 		password(len_min,len_max,text) {
 			 return [
 		        { required: true, message: text, trigger: 'blur' },
-		        { min: len_min, max: len_max, message: '长度在 '+len_min+' 到 '+len_max+' 个字符', trigger: 'blur' }
+		        { validator:(rule,value,callback) =>{
+	        			if(value.match(/^[a-zA-Z0-9]{5,16}$/)){
+	        				callback();
+	        			}else{
+	        				callback("数字和字母6-16位")
+	        			}
+	        		},
+	        		trigger:'blur' 
+		        }
 			]
 		},
-
+        
+        //手机号验证
 		phone(){
 			return [
-	        		{ required: true, message: '请输入手机', trigger: 'blur' },
+	        		{ required: true, message: '请输入手机号', trigger: 'blur' },
 	          		{
 	                	validator: (rule, value, callback) => {
 		                    if (value.match(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/)){
 		                        callback();
 		                    } else {
-		                        callback("请输入正确的手机");
+		                        callback("请输入正确的手机号");
 		                    }
 	                	},
 	                	trigger: 'blur'
 	            	}
 	        	]
 		},
-
+        
+        
+        //姓名验证
 		truename(){
 			return [
 	    		{ required: true, message: '请输入姓名', trigger: 'blur' },
-	        	{ min: 2, max: 8, message: '长度在 2 到 8 个字符', trigger: 'blur' }
+	    		
+	        	{
+	                validator: (rule, value, callback) => {
+		                    if (value.match(/^[a-zA-Z\u4e00-\u9fa5]{2,8}$/)){
+		                        callback();
+		                    } else {
+		                        callback("只能是汉子或英文2-8位");
+		                    }
+	                	},
+	                	trigger: 'blur'
+	            	}
 	    	]
+		},
+		
+		//性别验证
+		gender(){
+			return [
+			    { required: true, message: '请选择性别', trigger: 'blur' }
+			]
 		}
 	},
 	
