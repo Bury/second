a7d6ef3c83232a7360c65df9fcab454be877d3a7
瘦瘦 <catherine.zw0911@gmail.com>
2018-08-24 14:15:17 +0800

    地址

--- src/config/API.js
+++ src/config/API.js
@@ -5,13 +5,13 @@ let DEBUG = false;
 let cancleHTTP = [];//取消请求头设置；
 // const apiUrl = 'http://test.yy_api.ibetwo.com/';
 if (process.env.NODE_ENV == 'development') {
-   apiUrl = "http://dev-api.yy.ibetwo.com/";
+   apiUrl = "https://dev-api.yy.ibetwo.com/";
    DEBUG = true;
 }else if(process.env.NODE_ENV == 'production'){
-   apiUrl = "http://api.yy.ibetwo.com/";
+   apiUrl = "https://api.yy.ibetwo.com/";
    DEBUG = false;
 }else if(process.env.NODE_ENV == 'testing'){
-   apiUrl = "http://test-api.yy.ibetwo.com/";
+   apiUrl = "https://test-api.yy.ibetwo.com/";
    DEBUG = false;
 }
 export default{
