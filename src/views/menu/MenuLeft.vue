e92903a3b0f33ed8314b6467b83bc53e19d7604e
瘦瘦 <catherine.zw0911@gmail.com>
2018-09-17 11:42:16 +0800

    ceshi

--- src/views/menu/MenuLeft.vue
+++ src/views/menu/MenuLeft.vue
@@ -46,7 +46,6 @@
         props:{
             isCollapse: Boolean,
             isShow: Boolean,
-          // active_text_color:'',
         },
         data(){
             return{
@@ -60,41 +59,41 @@
     	      },
         },
         created:function(){
-          this.menu();
+          // this.menu();
         	this.getUrl();
 
         },
         methods:{
-          menu() {
-            userApi.menu().then((res) => {
-              if(res.data.errno === 0){
-        		 for(let i=0;i<res.data.data.length;i++){
-        		 	  if(res.data.data[i].no_child === false){
-        		 	  	 res.data.data[i].front_url = String(i)
-        		 	  }
-        		 }
-        		 this.$data.tableData = res.data.data;
-
-        		 this.refreash();
-        	 }
-
-            })
-          },
+          // menu() {
+          //   userApi.menu().then((res) => {
+          //     if(res.data.errno === 0){
+        	// 	 for(let i=0;i<res.data.data.length;i++){
+        	// 	 	  if(res.data.data[i].no_child === false){
+        	// 	 	  	 res.data.data[i].front_url = String(i)
+        	// 	 	  }
+        	// 	 }
+        	// 	 this.$data.tableData = res.data.data;
+          //
+        	// 	 this.refreash();
+        	//  }
+          //
+          //   })
+          // },
           getUrl() {
             this.$data.currentMenu = this.$route.name;
           },
-          refreash(){
-            let arr = [];
-            arr.push('OrderNow');
-            for(let item of this.$data.tableData){
-              arr.push(item.front_url);
-            }
-            if(arr.indexOf(this.$data.currentMenu) == -1){
-              // window.location.href = 'https://test.yy.ibetwo.com/';
-              this.$message.warning('该账号的岗位状态有更改，需重新登录');
-              globalFunctions.functions.user.logout(userApi,this.$router,this.$message);
-            }
-          },
+          // refreash(){
+          //   let arr = [];
+          //   arr.push('OrderNow');
+          //   for(let item of this.$data.tableData){
+          //     arr.push(item.front_url);
+          //   }
+          //   if(arr.indexOf(this.$data.currentMenu) == -1){
+          //     // window.location.href = 'https://test.yy.ibetwo.com/';
+          //     this.$message.warning('该账号的岗位状态有更改，需重新登录');
+          //     globalFunctions.functions.user.logout(userApi,this.$router,this.$message);
+          //   }
+          // },
         },
     }
 </script>
